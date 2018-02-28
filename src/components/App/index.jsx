6efdea93106b3258e79b62f2d97e0c966cce2614
Robert Lathrop <rldifts@gmail.com>
2018-02-28 18:20:47 -0500

    Add purple hex background & fill screen.

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -4,11 +4,8 @@ import './styles.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
+      <div className="BountyManager hex-background">
+        <p className="BountyManager-Intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
 import App from './components/App';
-import {unregister} from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 unregister();
