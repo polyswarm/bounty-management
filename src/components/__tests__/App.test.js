99deb81ed5f12c5c2e34607a2f2548d19787918f
Robert Lathrop <rldifts@gmail.com>
2018-02-28 16:23:55 -0500

    Setup enzyme tests.

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Adapter from 'enzyme-adapter-react-16'
+import {render} from 'enzyme';
+import {renderToJson} from 'enzyme-to-json'
 import App from '../App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const wrapper = render(<App />);
+  expect(renderToJson(wrapper)).toMatchSnapshot();
 });
