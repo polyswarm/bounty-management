7e273f04c4ece956689b6c8ecaa2079aa650d8ee
Robert Lathrop <rldifts@gmail.com>
2018-03-02 23:49:14 -0500

    Update file list tests.

--- src/components/__tests__/FileList.test.js
+++ src/components/__tests__/FileList.test.js
@@ -1,9 +1,40 @@
 import React from 'react';
-import {render} from 'enzyme';
-import {renderToJson} from 'enzyme-to-json';
+import {render, mount} from 'enzyme';
+import {renderToJson, mountToJson} from 'enzyme-to-json';
 import FileList from '../FileList';
 
+it('throws when created without files', () => {
+  expect(() => { render(<FileList />);  }).toThrow();
+});
+
 it('renders without crashing', () => {
-  const wrapper = render(<FileList />);
+  const files = [];
+  const wrapper = render(<FileList files={files} />);
   expect(renderToJson(wrapper)).toMatchSnapshot();
+});
+
+it('contains 3 FileProgress when 3 added', () => {
+  const files = [
+    {name: 'asdf'},
+    {name: 'demo'},
+    {name: 'test'},
+  ];
+  const wrapper = render(<FileList files={files} />);
+  expect(wrapper.find('.FileProgress')).toHaveLength(3);
+  expect(renderToJson(wrapper)).toMatchSnapshot();
+});
+
+it('calls remove when FileProgress remove button clicked',() => {
+  const remove = jest.fn();
+  const files = [
+    {name: 'asdf'},
+  ];
+  const wrapper = mount(
+    <FileList
+      removeFile={remove}
+      files={files} />
+  );
+  wrapper.find('button').simulate('click');
+  expect(remove).toHaveBeenCalled();
+  expect(mountToJson(wrapper)).toMatchSnapshot();
 });
