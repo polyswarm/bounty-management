332542405c330951b97e948a62cc96a716ddbb08
Robert Lathrop <rldifts@gmail.com>
2018-03-05 19:33:22 -0500

    Add jest fetch mock.

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,6 @@
 import {configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import fetch from 'jest-fetch-mock';
 
 configure({adapter: new Adapter()});
+global.fetch = fetch;
