585d5999352a2ebdee39087f430df15049365032
Robert Lathrop <rldifts@gmail.com>
2018-03-07 16:22:43 -0500

    Add backend as environment variable.

---
+++ src/components/App/config.js
@@ -0,0 +1,4 @@
+const config = {
+  url: process.env.REACT_APP_HOST,
+};
+export default config;
--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -3,20 +3,21 @@ import React, { Component } from 'react';
 // Bounty imports
 import BountyCreate from '../BountyCreate';
 // Component imports
+import config from './config';
 import './styles.css';
 
 class App extends Component {
   render() {
+    const {url} = config;
     return (
       <div className='BountyManager hex-background'>
         <p className='BountyManager-Intro'>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <BountyCreate />
+        <BountyCreate url={url}/>
       </div>
     );
   }
-
 }
 
 export default App;
