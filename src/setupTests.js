a2debf30eaeafadda2c340dd1eaf3a07ee39d843
Robert Lathrop <rldifts@gmail.com>
2018-03-10 15:30:16 -0500

    Clear warning.

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,7 +1,7 @@
 import {configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import fetch from 'jest-fetch-mock';
-import localStorage from './components/__mocks__/localstorage'
+import localStorage from './components/__mocks__/localstorage';
 
 configure({adapter: new Adapter()});
 global.fetch = fetch;
