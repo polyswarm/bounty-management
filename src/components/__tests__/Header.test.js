58306cc9f53f88a1d8d588afbabdc49a8f0e8513
Robert Lathrop <rldifts@gmail.com>
2018-03-12 15:31:34 -0400

    More tests.

--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -7,3 +7,11 @@ it('renders without crashing', () => {
   const wrapper = render(<Header />);
   expect(renderToJson(wrapper)).toMatchSnapshot();
 });
+
+it('shows title props', () => {
+  const wrapper = render(<Header title={'Title'}/>);
+
+  expect(wrapper.find('h1').text()).toBe('Title');
+
+  expect(renderToJson(wrapper)).toMatchSnapshot();
+});
