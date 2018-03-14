7d6737f53b60661c3239d552c90cbd5ef70ee59b
Robert Lathrop <rldifts@gmail.com>
2018-03-14 08:45:48 -0400

    Capitalize Polyswarm.

--- src/components/Welcome/strings.js
+++ src/components/Welcome/strings.js
@@ -1,7 +1,7 @@
 const strings = {
   welcome: 'Welcome to Polyswarm,',
   decentralized: 'The worlds first decentralized marketplace',
-  moreInfo: 'Simply upload files to create your own bounties and then let the microengines on polyswarm analyze your file.',
+  moreInfo: 'Simply upload files to create your own bounties and then let the microengines on Polyswarm analyze your file.',
   getStarted: 'Get Started!',
 };
 
