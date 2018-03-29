838c4369dee95221ddf50732387fabc93d53fe4a
Robert Lathrop <rldifts@gmail.com>
2018-03-28 20:22:06 -0400

    Fix tests for ModalPassword.

--- src/components/ModalPassword/index.jsx
+++ src/components/ModalPassword/index.jsx
@@ -19,7 +19,7 @@ class ModalPassword extends Component {
       password: '',
       address: 0,
       eth: 0,
-      nct: 0,
+      nct: 0
     };
 
     this.onWalletChangeHandler = this.onWalletChangeHandler.bind(this);
@@ -49,79 +49,74 @@ class ModalPassword extends Component {
     const { address: prevAddr } = prevState;
     const { walletList: prevWallets } = prevProps;
 
-    if (walletList.length > 0 && (prevAddr !== address ||
-      JSON.stringify(walletList) !== JSON.stringify(prevWallets))) {
+    if (
+      walletList &&
+      walletList.length > 0 &&
+      (prevAddr !== address ||
+        JSON.stringify(walletList) !== JSON.stringify(prevWallets))
+    ) {
       this.updateBalance(walletList[address]);
     }
   }
 
   render() {
     const { props: { walletList } } = this;
-    const { state: { open, unlocking, error, password, address, nct, eth } } = this;
+    const {
+      state: { open, unlocking, error, password, address, nct, eth }
+    } = this;
     return (
-      <div className='ModalPassword'>
+      <div className="ModalPassword">
         {open && (
           <React.Fragment>
-            <div className='ModalBackground'
-              onClick={this.onCloseClick}>
-            </div>
-            <div className='ModalContent'>
-              <header className='ModalContentHeader'>
+            <div className="ModalBackground" onClick={this.onCloseClick} />
+            <div className="ModalContent">
+              <header className="ModalContentHeader">
                 {walletList.length > 0 ? strings.header : strings.createHeader}
               </header>
               <form>
                 {walletList.length > 0 && (
                   <React.Fragment>
-                    <label htmlFor='address'>
-                      {strings.address}
-                    </label>
-                    <select id='address'
+                    <label htmlFor="address">{strings.address}</label>
+                    <select
+                      id="address"
                       value={walletList[address]}
-                      onChange={this.onChangeAddress}>
-                      {
-                        walletList.map((wallet) => {
-                          return(
-                            <option key={wallet}
-                              value={wallet}>
-                              {wallet}
-                            </option>
-                          );
-                        })
-                      }
+                      onChange={this.onChangeAddress}
+                    >
+                      {walletList.map(wallet => {
+                        return (
+                          <option key={wallet} value={wallet}>
+                            {wallet}
+                          </option>
+                        );
+                      })}
                     </select>
                     <label>Balances</label>
-                    <div className='Balances'>
-                      <p className='Balance'>NCT: {nct}</p>
-                      <p className='Balance'>ETH: {eth}</p>
+                    <div className="Balances">
+                      <p className="Balance">NCT: {nct}</p>
+                      <p className="Balance">ETH: {eth}</p>
                     </div>
                   </React.Fragment>
                 )}
-                <label htmlFor='password'>
-                  {strings.password}
-                </label>
-                <input id='password'
-                  type='password'
+                <label htmlFor="password">{strings.password}</label>
+                <input
+                  id="password"
+                  type="password"
                   value={password}
-                  onChange={this.onChangePassword}/>
-                <div className='ModalError'>
-                  {error && (
-                    strings.error
-                  )}
-                </div>
+                  onChange={this.onChangePassword}
+                />
+                <div className="ModalError">{error && strings.error}</div>
               </form>
-              <p className='ModalMessage'>{strings.background}</p>
-              <span className='Modal-Button-Bar'>
-                <Button
-                  disabled={unlocking}
-                  flat
-                  onClick={this.onUnlockClick}>
+              <p className="ModalMessage">{strings.background}</p>
+              <span className="Modal-Button-Bar">
+                <Button disabled={unlocking} flat onClick={this.onUnlockClick}>
                   {walletList.length > 0 ? strings.unlock : strings.create}
                 </Button>
                 <Button
                   flat
                   cancel
                   disabled={unlocking}
-                  onClick={this.onCloseClick}>
+                  onClick={this.onCloseClick}
+                >
                   {strings.cancel}
                 </Button>
               </span>
@@ -133,25 +128,25 @@ class ModalPassword extends Component {
   }
 
   onWalletChangeHandler() {
-    const { props: { onWalletChange }, state: {store} } = this;
+    const { props: { onWalletChange }, state: { store } } = this;
     if (onWalletChange) {
       onWalletChange(store);
     }
   }
 
   onChangeStore(event) {
-    this.setState({store: event.target.checked});
+    this.setState({ store: event.target.checked });
   }
 
   onChangePassword(event) {
-    this.setState({password: event.target.value});
+    this.setState({ password: event.target.value });
   }
 
   onChangeAddress(event) {
     const { props: { walletList } } = this;
     const value = event.target.value;
-    const index = walletList.findIndex((v) => v === value);
-    this.setState({address: index});
+    const index = walletList.findIndex(v => v === value);
+    this.setState({ address: index });
   }
 
   onCloseClick() {
@@ -169,66 +164,63 @@ class ModalPassword extends Component {
     } else {
       this.createWallet(password);
     }
-
   }
 
   onUnlock(address, password) {
     const { props: { url } } = this;
-    this.setState({unlocking: true, error: false});
+    this.setState({ unlocking: true, error: false });
     const http = new HttpAccount(url);
-    http.unlockWallet(address, password)
-      .then(success => {
-        this.setState({unlocking: false, error: !success});
-        if (success) {
-          this.onWalletChangeHandler();
-          this.close();
-        }
-      });
+    http.unlockWallet(address, password).then(success => {
+      this.setState({ unlocking: false, error: !success });
+      if (success) {
+        this.onWalletChangeHandler();
+        this.close();
+      }
+    });
   }
 
   createWallet(password) {
     const { props: { url } } = this;
-    this.setState({unlocking: true, error: false});
+    this.setState({ unlocking: true, error: false });
     const http = new HttpAccount(url);
-    http.createWallet(password)
-      .then(success => {
-        this.setState({unlocking: false, error: !success});
-        if (success) {
-          this.onWalletChangeHandler();
-          this.close();
-        }
-      });
+    http.createWallet(password).then(success => {
+      this.setState({ unlocking: false, error: !success });
+      if (success) {
+        this.onWalletChangeHandler();
+        this.close();
+      }
+    });
   }
 
   open() {
-    this.setState({open: true});
+    this.setState({ open: true });
   }
 
   close() {
-    this.setState({open: false});
+    this.setState({ open: false });
   }
 
   updateBalance(address) {
     const { props: { url } } = this;
     const http = new HttpAccount(url);
-    http.getEth(address)
-      .then(balance => {
-        const b = new BigNumber(balance)
-          .dividedBy(new BigNumber(1000000000000000000));
-        this.setState({eth: b.toNumber()});
-      });
-    http.getNct(address)
-      .then(balance => {
-        const b = new BigNumber(balance)
-          .dividedBy(new BigNumber(1000000000000000000));
-        this.setState({nct: b.toNumber()});
-      });
+    http.getEth(address).then(balance => {
+      const b = new BigNumber(balance).dividedBy(
+        new BigNumber(1000000000000000000)
+      );
+      this.setState({ eth: b.toNumber() });
+    });
+    http.getNct(address).then(balance => {
+      const b = new BigNumber(balance).dividedBy(
+        new BigNumber(1000000000000000000)
+      );
+      this.setState({ nct: b.toNumber() });
+    });
   }
 }
 
 ModalPassword.proptypes = {
   url: PropTypes.string,
   walletList: PropTypes.array,
-  onWalletChange: PropTypes.func,
+  onWalletChange: PropTypes.func
 };
 export default ModalPassword;
