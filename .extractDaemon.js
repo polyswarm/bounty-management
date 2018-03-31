1fbea13931a26caa6c0d5496f555273a897305bb
Robert Lathrop <rldifts@gmail.com>
2018-03-31 02:11:14 -0400

    Revert "Break linux version out."
    This reverts commit fbbb07571c6b462c5eb1ce9b28006643215c9951 [formerly cc655216cc3f361d3c71a86e695c5d2e0edfe11f].
    
    
    Former-commit-id: 12ca8b9aecab15cb92678b09f03d200f0d41dd1d

--- .extractDaemon.js
+++ .extractDaemon.js
@@ -1,46 +1,44 @@
-const fs = require('fs');
-const path = require('path');
-
 module.exports = (buildPath, electronVersion, platform, arch, callback) => {
-  let tail;
-  switch (platform) {
-    case 'linux':
-      breakoutLinux(buildPath);
-      break;
-    case 'win32':
-    case 'darwin':
-    default:
-      throw Error(`Application does not support platform ${platform}`);
-  }
-}
+    const fs = require('fs');
+    const tar = require('tar');
+    const path = require('path');
+    const rimraf = require('rimraf');
+    const deasync = require('deasync');
 
-breakoutLinux = (buildPath) => {
-  const tar = require('tar');
-  const rimraf = require('rimraf');
-  const deasync = require('deasync');
+    const tarXSync = deasync(tar.x);
+    const rimrafSync = deasync(rimraf);
 
-  const tarXSync = deasync(tar.x);
-  const rimrafSync = deasync(rimraf);
+    let tail;
+    switch (platform) {
+      case 'linux':
+        tail = '-x86_64-linux.tar.gz'
+        break;
+      case 'win32':
+      case 'darwin':
+      default:
+        throw Error(`Application does not support platform ${platform}`);
+    }
 
-  const filename = 'polyswarmd-electron-x86_64-linux.tar.gz';
-  const app = path.resolve(buildPath);
-  const backend = path.resolve(app, 'backend');
-  const polyswarmd = path.resolve(app,  'polyswarmd');
+    
+    const filename = 'polyswarmd-electron-x86_64-linux.tar.gz';
+    const app = path.resolve(buildPath);
+    const backend = path.resolve(app, 'backend');
+    const polyswarmd = path.resolve(app,  'polyswarmd');
 
-  rimrafSync(polyswarmd);
-  fs.mkdirSync(polyswarmd);
-  tarXSync({
-    file: path.resolve(backend, filename),
-    strip: 1,
-    C: polyswarmd
-  });
+    rimrafSync(polyswarmd);
+    fs.mkdirSync(polyswarmd);
+    tarXSync({
+      file: path.resolve(backend, filename),
+      strip: 1,
+      C: polyswarmd
+    })
 
-  const dest = path.resolve(polyswarmd, 'polyswarmd.cfg');
-  const cfg = path.resolve(backend, 'polyswarmd.cfg');
-  fs.copyFileSync(cfg, dest);
-  if (buildPath.match(/[\\/]tmp/)) {
-    rimraf(backend, () => {
-      callback();
-    });
-  }
+    const dest = path.resolve(polyswarmd, 'polyswarmd.cfg');
+    const cfg = path.resolve(backend, 'polyswarmd.cfg');
+    fs.copyFileSync(cfg, dest);
+    if (buildPath.match(/[\\/]tmp/)) {
+      rimraf(backend, () => {
+        callback();
+      });
+    }
 }
