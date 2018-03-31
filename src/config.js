a0d8c2ee11a9dcf94c0c8d6ae4e36d961671c769
Robert Lathrop <rldifts@gmail.com>
2018-03-30 20:25:35 -0400

    Extract daemon during build/start process. Spawn process with extracted file in electron.

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 const config = {
   host: 'http://localhost:8080',
   websocket_host: 'ws://localhost:8080/events',
-  archive: 'polyswarmd-electron',
   daemon: 'polyswarmd'
 };
 export default config;
