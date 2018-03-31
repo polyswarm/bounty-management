6bba777de9845c2d9d0000facff585698f91b139
Robert Lathrop <rldifts@gmail.com>
2018-03-30 21:02:33 -0400

    Fix issue where make would fail after start.
    Former-commit-id: ee7039b80398f403c028cbf5e79e39b0d12b246a

--- .buildDaemon.js
+++ .buildDaemon.js
@@ -1,8 +1,4 @@
 const extract = require('./.extractDaemon.js');
-const rimraf = require('rimraf');
 const path = require('path');
-const deasync = require('deasync');
 
-const rimrafSync = deasync(rimraf);
-rimrafSync(path.resolve(process.cwd(), 'polyswarmd'));
 extract(process.cwd(), 0, 'linux', '', () => {});
