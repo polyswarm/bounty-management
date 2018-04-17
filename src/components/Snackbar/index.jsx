43f73a4ed34f28166011001920c7b590c4fb2fd5
Robert Lathrop <rldifts@gmail.com>
2018-04-17 13:01:02 -0400

    Add dismiss button to Snackbar.

--- src/components/Snackbar/index.jsx
+++ src/components/Snackbar/index.jsx
@@ -2,23 +2,39 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 // Bounty Manager imports
-
+import Button from '../Button';
+// Component imports
+import strings from './strings';
 class Snackbar extends Component {
   constructor(props) {
     super(props);
+    this.dismiss = this.dismiss.bind(this);
   }
 
   render() {
     const { message } = this.props;
     return(
-      <p className='Snackbar'>
-        {message}
-      </p>
+      <div className='Snackbar'>
+        <p>
+          {message}
+        </p>
+        <Button flat
+          onClick={this.dismiss}>
+          {strings.dismiss}
+        </Button>
+      </div>
     );
   }
 
+  dismiss() {
+    const { onDismiss } = this.props;
+    if (onDismiss) {
+      onDismiss();
+    }
+  }
 }
 Snackbar.proptypes = {
-  message: PropTypes.string
+  message: PropTypes.string,
+  onDismiss: PropTypes.func
 };
 export default Snackbar;
