b0071e378cd27c1040faff22c74d8065729a3660
Robert Lathrop <rldifts@gmail.com>
2018-04-26 14:54:08 -0400

    Revert to Get Started text.
    Former-commit-id: 1671d4a8ff71b216fa6a0a25699adc98e23c1ed0

--- src/components/Welcome/strings.js
+++ src/components/Welcome/strings.js
@@ -2,7 +2,7 @@ const strings = {
   welcome: 'Welcome to the Polyswarm Alpha,',
   decentralized: 'The worlds first decentralized antivirus marketplace',
   moreInfo: 'Use at your own risk: Bugs may result in a permanent loss of Nectar (NCT).',
-  getStarted: 'I understand the risk. Get to it!',
+  getStarted: 'Get Started!',
   accept: 'I understand and accept the risks involved in using the alpha product.',
 };
 
